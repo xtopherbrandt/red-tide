ca30516438b8c8e8b08d9490875ff7218687fa92
Christopher Brandt <xtopher.brandt@gmail.com>
2015-03-12 23:54:10 +0000

    first draft

---
+++ thesmartcellar.py
@@ -0,0 +1,34 @@
+import cgi
+import os
+import urllib
+import logging
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+from google.appengine.api import urlfetch
+from google.appengine.api import taskqueue
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+
+      template_values = {
+              
+      }
+
+      template = JINJA_ENVIRONMENT.get_template('index.html')
+      self.response.write(template.render(template_values))
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
+
